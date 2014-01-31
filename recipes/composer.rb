1612b5c13ae0ba042ad64ff99aa9c40dd8aca241
Pirate Dunbar <yarr@piratedunbar.com>
2014-01-31 13:32:29 -0500

    Update composer.rb

--- recipes/composer.rb
+++ recipes/composer.rb
@@ -11,3 +11,11 @@ remote_file "#{node['php']['composer']['install_path']}/composer" do
 	mode   '0755'
 	not_if { File.exists?("#{node['php']['composer']['install_path']}/composer") }
 end
+
+bash "globally install composer packages" do
+  code <<-EOH
+      composer global require phing/phing:2.6.*
+      composer global require phpunit:phpunit/3.7.*
+      composer global require wp-cli/wp-cli:0.13
+  EOH
+end
