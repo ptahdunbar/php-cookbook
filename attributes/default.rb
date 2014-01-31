339a920d834c6cdecd94b3cba783e2d706b72bc2
Pirate Dunbar <yarr@piratedunbar.com>
2014-01-30 22:44:02 -0500

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -25,7 +25,7 @@ default[:php][:request_slowlog_timeout] = '10s'
 
 
 default[:php][:dependencies]  				= %w(libcurl4-openssl-dev libpq-dev libreadline-dev openssl libssl-dev libxslt1-dev zlib1g-dev libbz2-dev libc-client2007e-dev libkrb5-dev libcurl4-gnutls-dev libfreetype6-dev libgmp3-dev libjpeg8-dev libmcrypt-dev libpng12-dev libt1-dev libmhash-dev libexpat1-dev libicu-dev libtidy-dev re2c lemon)
-default[:php][:packages] 					= %w(php5-fpm php5-cli php5-common php5-dev memcached php5-memcached php5-imagick php5-xdebug php5-mcrypt php5-mysql php5-imap php5-curl php-pear php5-gd php-apc)
+default[:php][:packages] 					= %w(php5-fpm php5-cli php5-common php5-dev php5-imagick php5-xdebug php5-mcrypt php5-mysql php5-imap php5-curl php-pear php5-gd php-apc)
 
 default[:php][:composer][:install_path] 	= '/usr/local/bin'
 
